e6335a43d9bf251343007468d40254d383b38e94
Jakob Olsen <jolsen16@charlotte.edu>
2024-11-18 15:21:47 -0500

    Modified order schema

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -1,13 +1,19 @@
 from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
-from .order_details import OrderDetail
 
+from .sandwiches import Sandwich
+from .restaurants import Restaurant
 
 
 class OrderBase(BaseModel):
-    customer_name: str
+    id: int
+    user_id: int
+    order_date: datetime
     description: Optional[str] = None
+    sandwich_id: int
+    amount: float
+    restaurant_id: int
 
 
 class OrderCreate(OrderBase):
@@ -15,14 +21,25 @@ class OrderCreate(OrderBase):
 
 
 class OrderUpdate(BaseModel):
-    customer_name: Optional[str] = None
-    description: Optional[str] = None
+    id: Optional[int]
+    user_id: Optional[int]
+    order_date: Optional[datetime]
+    description: Optional[str]
+    sandwich_id: Optional[int]
+    amount: Optional[float]
+    restaurant_id: Optional[int]
 
 
 class Order(OrderBase):
     id: int
+    user_id: int
+    order_date: datetime
+    description: Optional[str] = None
+    sandwich_id: Sandwich = None
     order_date: Optional[datetime] = None
-    order_details: list[OrderDetail] = None
+    amount: float
+    restaurant_id: Restaurant = None
+
 
     class ConfigDict:
         from_attributes = True
