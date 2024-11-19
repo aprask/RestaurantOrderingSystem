ac870aa18ea54a8ba1e6a2bb92d98cbbab2d51bb
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-11-19 07:52:39 -0500

    Removed plain text from most fields in config.py file for security and conflict-free merges

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 class conf:
-    host = "127.0.0.1"
-    database = "sandwich_maker_api"
-    port = 3306
-    user = "root"
-    password = "root"
-    app_host = "localhost"
-    app_port = 8000
+    host = os.getenv("DB_HOST", "127.0.0.1")
+    database = os.getenv("DB_DATABASE", "sandwich_maker_api")
+    port = int(os.getenv("DB_PORT", 3306))
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    app_host = os.getenv("APP_HOST", "localhost")
+    app_port = int(os.getenv("APP_PORT", 8000))
