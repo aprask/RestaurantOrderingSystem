d19aaebe29e517f9717b30505265b6c84c1233bb
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-11-19 07:58:43 -0500

    Updated test_db_connect.py w/ os env vars

--- api/tests/test_db_connect.py
+++ api/tests/test_db_connect.py
@@ -1,13 +1,16 @@
 import pymysql
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 if __name__ == "__main__":
     try:
         connection = pymysql.connect(
-            host='127.0.0.1',
-            user='root',
-            password='root',
-            database='sandwich_maker_api',
-            port=3306
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_DATABASE"),
+            port=int(os.getenv("DB_PORT", 3306))
         )
         print("Connection successful!")
         connection.close()
