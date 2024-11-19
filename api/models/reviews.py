28e8748053fcd4441bd9895f8cda451259814ee3
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-11-19 08:15:30 -0500

    Update: reviews.py model to have a nullable description

--- api/models/reviews.py
+++ api/models/reviews.py
@@ -11,7 +11,7 @@ class Review(Base):
     order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
     restaurant_id = Column(Integer, ForeignKey("restaurants.id"), nullable=False)
     rating = Column(Integer, nullable=False)
-    description = Column(String(500), nullable=False)
+    description = Column(String(500), nullable=True)
 
     order = relationship("Order", back_populates="reviews")
     restaurant = relationship("Restaurant", back_populates="reviews")
