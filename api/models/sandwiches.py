6a7ce58509c174234d4c48d245c4b1dfcd533fd5
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-11-23 21:06:31 -0500

    Updated sandwiches model and schema to include more columns

--- api/models/sandwiches.py
+++ api/models/sandwiches.py
@@ -8,8 +8,9 @@ class Sandwich(Base):
     __tablename__ = "sandwiches"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    sandwich_name = Column(String(100), unique=True, nullable=True)
+    sandwich_name = Column(String(100), unique=True, nullable=True, server_default="null_sandwich")
     price = Column(DECIMAL(4, 2), nullable=False, server_default='0.0')
+    calories = Column(Integer, nullable=False, server_default='0')
 
     recipes = relationship("Recipe", back_populates="sandwich")
     order_details = relationship("OrderDetail", back_populates="sandwich")
--- api/schemas/sandwiches.py
+++ api/schemas/sandwiches.py
@@ -1,12 +1,17 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel
 
 
 class SandwichBase(BaseModel):
     sandwich_name: str
     price: float
-
+    calories: float
+    sandwich_category: str
+    sandwich_size: str
+    is_vegetarian: bool
+    is_vegan: bool
+    is_gluten_free: bool
 
 class SandwichCreate(SandwichBase):
     pass
@@ -15,12 +20,24 @@ class SandwichCreate(SandwichBase):
 class SandwichUpdate(BaseModel):
     sandwich_name: Optional[str] = None
     price: Optional[float] = None
+    calories: Optional[float] = None
+    sandwich_category: Optional[str] = None
+    sandwich_size: Optional[str] = None
+    is_vegetarian: Optional[bool] = None
+    is_vegan: Optional[bool] = None
+    is_gluten_free: Optional[bool] = None
 
 
 class Sandwich(SandwichBase):
     id: int
     sandwich_name: str
     price: float
+    calories: float
+    sandwich_category: str
+    sandwich_size: str
+    is_vegetarian: bool
+    is_vegan: bool
+    is_gluten_free: bool
 
     class ConfigDict:
         from_attributes = True
