49fc1f299c2eca14c8ac81590c1366de8fffc97c
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-11-23 21:15:44 -0500

    Updated restaurant model to establish a relationship with order model

--- api/models/restaurants.py
+++ api/models/restaurants.py
@@ -9,3 +9,4 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     restaurant_name = Column(String(500))
+    orders = relationship("Order", back_populates="restaurant")
