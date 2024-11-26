339f5686eeecd72aa954bf1f022bd815b1001138
Jakob Olsen <jolsen618@gmail.com>
2024-11-25 22:36:36 -0500

    Added router for reviews

---
+++ api/routers/reviews.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, FastAPI, status, Response
+from sqlalchemy.orm import Session
+from ..controllers import reviews as controller
+from ..schemas import reviews as schema
+from ..dependencies.database import engine, get_db
+
+router = APIRouter(
+    tags=["Reviews"],
+    prefix="/reviews"
+)
+
+@router.post("/", response_model=schema.Review)
+def create(request: schema.ReviewCreate, db: Session = Depends(get_db)):
+    return controller.create(db, request)
+
+@router.get("/", response_model=list[schema.Review])
+def read_all(db: Session = Depends(get_db)):
+    return controller.read_all(db)
+
+@router.get("/{review_id}", response_model=schema.Review)
+def read_one(review_id: int, db: Session = Depends(get_db)):
+    return controller.read_one(db, review_id)
+
+@router.put("/{review_id}", response_model=schema.Review)
+def update(review_id: int, request: schema.ReviewUpdate, db: Session = Depends(get_db)):
+    return controller.update(db, review_id, request)
+
+@router.delete("/{review_id}", response_model=schema.Review)
+def delete(review_id: int, db: Session = Depends(get_db)):
+    return controller.delete(db, review_id)
