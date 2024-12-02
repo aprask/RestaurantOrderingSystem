89c1a5d7dc90bda1f97933bbfb85e8a737a9f06a
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-12-02 09:26:27 -0500

    Added endpoints for ordering rating in reviews

--- api/controllers/reviews.py
+++ api/controllers/reviews.py
@@ -64,3 +64,19 @@ def delete(db: Session, review_id):
         error = str(e.__dict__["orig"])
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+def sort_reviews_by_rating_asc(db: Session):
+    try:
+        result = db.query(model.Review).order_by(model.Review.rating.asc()).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__["orig"])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return result
+
+def sort_reviews_by_rating_dsc(db: Session):
+    try:
+        result = db.query(model.Review).order_by(model.Review.rating.desc()).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__["orig"])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return result
