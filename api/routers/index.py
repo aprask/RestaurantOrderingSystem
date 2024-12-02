c7dbc25da99adc6e9db56efe786f1f232e93ce92
Music-Mad <chrisklepper1776@gmail.com>
2024-12-02 15:29:29 -0500

    Added restaurant controller and route

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, coupons, users, reviews
+from . import orders, coupons, users, reviews, restaurants
 
 
 def load_routes(app):
@@ -6,3 +6,4 @@ def load_routes(app):
     app.include_router(coupons.router)
     app.include_router(users.router)
     app.include_router(reviews.router)
+    app.include_router(restaurants.router)
