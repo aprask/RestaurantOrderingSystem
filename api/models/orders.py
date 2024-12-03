28b64cb7ffcadeeb2a3c628831d348b540dea48d
Devin Thompson <dthom118@charlotte.edu>
2024-12-02 22:00:45 -0500

    fix: fixed type with relationship for reviews, added promotion_code to have a foreignkey relationship to coupons which correspond to the id of the coupon designated in coupons table

--- api/models/orders.py
+++ api/models/orders.py
@@ -16,9 +16,10 @@ class Order(Base):
     restaurant_id = Column(Integer, ForeignKey("restaurants.id"), nullable=False)
     delivery_method = Column(String(300), nullable=False)
     status_of_order = Column(String(300), nullable=False, server_default="pending")
-    promotion_code = Column(String(300), nullable=True, server_default="N/A")
+    promotion_code = Column(String(300), ForeignKey("coupons.id"), nullable=True, server_default="N/A")
 
     sandwich = relationship("Sandwich", back_populates="orders")
     restaurant = relationship("Restaurant", back_populates="orders")
     user = relationship("User", back_populates="orders")
-    reviews = relationship("Review", back_populates="order")
+    reviews = relationship("Review", back_populates="orders")
+    coupons = relationship("Coupon", back_populates="orders")
