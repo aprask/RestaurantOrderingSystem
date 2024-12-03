0e2dc8d11bff3ebea578625196c2902ce78327fa
Jakob Olsen <jolsen618@gmail.com>
2024-12-02 22:10:03 -0500

    Updated test_reviews.py to include necessary data in the mock database

--- api/tests/test_reviews.py
+++ api/tests/test_reviews.py
@@ -4,6 +4,16 @@ from ..main import app
 import pytest
 from ..models import reviews as model
 
+# Models and controllers for order, etc
+from ..models import orders as order_model
+from ..controllers import orders as order_controller
+from ..models import restaurants as restaurant_model
+from ..controllers import restaurants as restaurant_controller
+from ..models import users as user_model
+from ..controllers import users as user_controller
+from ..models import sandwiches as sandwich_model
+from ..controllers import sandwiches as sandwich_controller
+
 # Create a test client for the app
 client = TestClient(app)
 
@@ -14,10 +24,54 @@ def db_session(mocker):
 
 
 def test_create_review(db_session):
+
+    # Create order and restaurant objects for mock db
+    order_data = {
+        "user_id": 1,
+        "description": "Test order",
+        "sandwich_id": 1,
+        "amount": 10.00,
+        "restaurant_id": 1,
+        "delivery_method": "pickup",
+        "status_of_order": "pending",
+    }
+
+    # Order requires a restaurant, user and sandwich model
+    user_data = {
+        "customer_name": "Test user",
+        "payment_method": "cash"
+    }
+
+    sandwich_data = {
+        "sandwich_name": "Test sandwich",
+        "description": "Test description",
+        "price": 10.00,
+        "calories": 100,
+        "sandwich_size": "small",
+        "is_vegetarian": True,
+        "is_vegan": False,
+        "is_gluten_free": False
+    }
+
+    restaurant_data = {
+        "restaurant_name": "(S)Andrew's Sandwiches"
+    }
+
+    user_object = user_model.User(**user_data)
+    sandwich_object = sandwich_model.Sandwich(**sandwich_data)
+    restaurant_object = restaurant_model.Restaurant(**restaurant_data)
+    order_object = order_model.Order(**order_data)
+
+    user_controller.create(db_session, user_object)
+    sandwich_controller.create(db_session, sandwich_object)
+    restaurant_controller.create(db_session, restaurant_object)
+    order_controller.create(db_session, order_object)
+
     # Create a sample review
     review_data = {
         "order_id": 1,
         "restaurant_id": 1,
+        "user_id": 1,
         "rating": 5,
         "description": "Test review"
     }
@@ -31,5 +85,6 @@ def test_create_review(db_session):
     assert created_review is not None
     assert created_review.order_id == 1
     assert created_review.restaurant_id == 1
+    assert created_review.user_id == 1
     assert created_review.rating == 5
     assert created_review.description == "Test review"
