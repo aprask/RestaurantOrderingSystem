922a85736de75fd96f0395513ebc48a91d8c31e8
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-12-03 09:19:40 -0500

    Resolved conflict in index.py

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, coupons, users, reviews, restaurants, recipes
+from . import orders, coupons, users, reviews, resources, sandwiches, restaurants, recipes
 
 
 def load_routes(app):
@@ -6,5 +6,7 @@ def load_routes(app):
     app.include_router(coupons.router)
     app.include_router(users.router)
     app.include_router(reviews.router)
+    app.include_router(resources.router)
+    app.include_router(sandwiches.router)
     app.include_router(recipes.router)
     app.include_router(restaurants.router)
