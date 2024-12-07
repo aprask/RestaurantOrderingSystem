6498fd39b17aa7aef44662d85b7b37950f9c03f1
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-12-07 16:45:03 -0500

    rearranged endpoints in routes in order to avoid default parsing for path variable

--- api/routers/reviews.py
+++ api/routers/reviews.py
@@ -17,10 +17,6 @@ def create(request: schema.ReviewCreate, db: Session = Depends(get_db)):
 def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
-@router.get("/{rest_id}", response_model=schema.Review)
-def get_reviews_from_restaurant(rest_id: int, db: Session = Depends(get_db)):
-    return controller.get_reviews_from_restaurant(db=db, rest_id=rest_id)
-
 @router.get("/order-asc", response_model=list[schema.Review])
 def order_rating_asc_order(db: Session = Depends(get_db)):
     return controller.sort_reviews_by_rating_asc(db)
@@ -28,6 +24,10 @@ def order_rating_asc_order(db: Session = Depends(get_db)):
 @router.get("/order-desc", response_model=list[schema.Review])
 def order_rating_asc_order(db: Session = Depends(get_db)):
     return controller.sort_reviews_by_rating_dsc(db)
+
+@router.get("/{rest_id}", response_model=schema.Review)
+def get_reviews_from_restaurant(rest_id: int, db: Session = Depends(get_db)):
+    return controller.get_reviews_from_restaurant(db=db, rest_id=rest_id)
 
 @router.get("/{review_id}", response_model=schema.Review)
 def read_one(review_id: int, db: Session = Depends(get_db)):
